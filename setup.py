db3ff24bb8720a5cec1344932be45102db1c974f
Joao Vitor Dias <joao.dias.santos223@gmail.com>
2020-10-22 02:37:58 -0300

    Added requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DatasetAnalysisProject',
+    version='1.0',
+    packages=['matplotlib, numpy, sympy, pandas, scipy, scikit-learn, setuptools'],
+    url='',
+    license='',
+    author='joao_vitor',
+    author_email='joao.santos.ismart@gmail.com',
+    description=''
+)
